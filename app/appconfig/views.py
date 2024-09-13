9cb954e48c399cd9edf1435eebd7dc458efd5de5
Shailesh Mamgain <shaileshmamgain5@gmail.com>
2024-09-13 18:12:20 -0400

    app config tests added

--- app/appconfig/views.py
+++ app/appconfig/views.py
@@ -1,13 +1,17 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from core.models import AppConfiguration
 from .serializers import AppConfigurationSerializer
+from core.permissions import IsStaffOrSuperuser
 
 class AppConfigurationListCreateView(generics.ListCreateAPIView):
     queryset = AppConfiguration.objects.all()
     serializer_class = AppConfigurationSerializer
-    permission_classes = [IsAdminUser]
 
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsStaffOrSuperuser()]
+        return [IsAuthenticated()]
 
 class ActiveAppConfigurationView(generics.RetrieveAPIView):
     serializer_class = AppConfigurationSerializer
